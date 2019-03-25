11ec10d3239a0c40498a765bb350e9635d212ea6
mikaelsoerensen <mikael.soerensen@roskilde-festival.dk>
2019-03-25 20:14:30 +0100

    startup script cleaned

--- server/functions/startup/index.js
+++ server/functions/startup/index.js
@@ -1,17 +1,22 @@
 // Firebase Setup
-
 const admin = require('firebase-admin');
-
 var db = admin.firestore();
 
 const keys = require('../serviceAccountKey');
 const request = require('request');
 
-const RestTeams = 'https://people-vol.roskilde-festival.dk/Api/MemberApi/1/GetTeams/?ApiKey=';
-
-var RestTeamBusBus = 'https://people-vol.roskilde-festival.dk/Api/MemberApi/1/GetTeamMembers/?teamId=6822&ApiKey=';
-
-//const Link = 'https://people-vol.roskilde-festival.dk/Api/MemberApi/1/GetMembersByTerm/?term=poul&ApiKey=';
+/*
+var userRef = db.collection('users').where('teams', 'array-contains', 6822);
+  
+userRef.get().then(snapshot => {
+      snapshot.docs.forEach(doc => {
+              console.log('Document data:', doc.id, doc.data());
+          })
+      }).catch(err => {
+        console.log('Error getting document', err);
+        return err;
+    });
+ */
 
 var requiredTeamId = [
     6822, // BUSBUS
@@ -52,10 +57,9 @@ requiredAdmins.forEach(function(entry) {
             
                 // Add user to firestore if admin
                 var addUser = db.collection('users').doc(`${PeopleData.Member.MemberId}`).set(tempUser).then(ref => {
-                    //console.log('Added document with ID: ', ref);
+                    //console.log('Added document', ref);
                 });
             }
-        //console.log('body:', PeopleData); // Print the HTML for the Google homepage.
         });
 });
 
@@ -86,60 +90,43 @@ requiredTeamId.forEach(function(entry) {
                 
                     // Add team to firestore if admin
                     var addTeam = db.collection('teams').doc(`${PeopleData.Teams[item].TeamId}`).set(tempTeam).then(ref => {
-                        //console.log('Added document with ID: ', ref);
+                        //console.log('Added document: ', ref);
                     });
                 }
         });
-        //console.log('body:', PeopleData); // Print the HTML for the Google homepage.
-        });
+    });
 
     // People REST API: GetTeamMember pr. teamID
     var RestTeam = 'https://people-vol.roskilde-festival.dk/Api/MemberApi/1/GetTeamMembers/?teamId=' + entry + '&ApiKey=';
 
     request(RestTeam + keys.RestAPIKey, function (error, response, body) {
-    //console.log('error:', error); // Print the error if one occurred
-    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    const PeopleData = JSON.parse(body);
-    module.exports = PeopleData;
+        //console.log('error:', error); // Print the error if one occurred
+        //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        const PeopleData = JSON.parse(body);
+        module.exports = PeopleData;
 
-    // Loop through a whole team
-    Object.keys(PeopleData.TeamMembers).forEach(function (item) {
-        
-            // Find users that are admins / "Holdleder". If not users is "basis"
-            if (PeopleData.TeamMembers[item].RoleName === 'Holdleder') {
-                //console.log('body:', PeopleData.TeamMembers[item]);
-
-                var tempUser = {
-                    email: PeopleData.TeamMembers[item].Email,
-                    name: PeopleData.TeamMembers[item].MemberName,
-                    peopleId: PeopleData.TeamMembers[item].MemberId,
-                    role: PeopleData.TeamMembers[item].RoleName,
-                    teams: [PeopleData.TeamMembers[item].TeamId]
-                };
+        // Loop through a whole team
+        Object.keys(PeopleData.TeamMembers).forEach(function (item) {
             
-                // Add user to firestore if admin
-                var addUser = db.collection('users').doc(`${PeopleData.TeamMembers[item].MemberId}`).set(tempUser).then(ref => {
-                    //console.log('Added document with ID: ', ref);
-                });
+                // Find users that are admins / "Holdleder". If not users is "basis"
+                if (PeopleData.TeamMembers[item].RoleName === 'Holdleder') {
+                    //console.log('body:', PeopleData.TeamMembers[item]);
 
-            }
-    });
-    //console.log('body:', PeopleData); // Print the HTML for the Google homepage.
-    });
+                    var tempUser = {
+                        email: PeopleData.TeamMembers[item].Email,
+                        name: PeopleData.TeamMembers[item].MemberName,
+                        peopleId: PeopleData.TeamMembers[item].MemberId,
+                        role: PeopleData.TeamMembers[item].RoleName,
+                        teams: [PeopleData.TeamMembers[item].TeamId]
+                    };
+                
+                    // Add user to firestore if admin
+                    var addUser = db.collection('users').doc(`${PeopleData.TeamMembers[item].MemberId}`).set(tempUser).then(ref => {
+                        //console.log('Added document with ID: ', ref);
+                    });
 
+                }
+        });
+
+    });
 });
-
-  var userRef = db.collection('users').where('teams', 'array-contains', '6822');
-  var getDoc = userRef.get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        console.log('Document data:', doc.data());
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-
-    
