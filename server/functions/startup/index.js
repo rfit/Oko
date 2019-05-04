7b3198ed9d3f4ce4fc860d1ff724c71b5bd772f2
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2019-05-04 20:48:13 +0200

    Change startup script
    Will create uids in db, with random not peopleid.
    
    will generate a photoUrl

--- server/functions/startup/index.js
+++ server/functions/startup/index.js
@@ -6,58 +6,62 @@ var FieldValue = require("firebase-admin").firestore.FieldValue;
 const keys = require('../serviceAccountKey');
 const request = require('request');
 
+function generatePhotoUrl(email) {
+    var crypto = require('crypto');
+    const hash = crypto.createHash('md5').update(email).digest("hex");
+    return 'https://www.gravatar.com/avatar/' + hash;
+}
+
 module.exports = function() {
     const data = require('../data/teams-2019');
     var requiredListOwnerEmail = data.requiredListOwnerEmail;
     var requiredAdmins = data.requiredAdmins;
 
+    console.log(`Starting import of ${requiredListOwnerEmail.length} owners, and ${requiredAdmins.length} admins.`);
+
     var arrayListId = [];
-    var j=0;
     var arrayMemberId = [];
-    var i=0;
     var itemsProcessed = 0;
 
     // Loop through all admins and create. 
     requiredListOwnerEmail.forEach(function(entry) {
-
         // People REST API: GetTeams (Get all teams in people)
         var RestMember = 'https://people-pro.roskilde-festival.dk/Api/MemberApi/1/GetMembersByTerm/?term=' + entry + '&ApiKey=';
 
         request(RestMember + keys.RestAPIKey, function (error, response, body) {
-                //console.log('error:', error); // Print the error if one occurred
+                if(error) {
+                    console.log('Create Error:', error); // Print the error if one occurred
+                }
                 //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                 const PeopleData = JSON.parse(body);
-                module.exports = PeopleData;
                 
                 // Loop through a whole team
-                Object.keys(PeopleData.Members).forEach(function (item) {
+                Object.keys(PeopleData.Members).forEach(function (item, i) {
                     
                     // Find users that are admins / "Holdleder". If not users is "basis"
                     if (PeopleData.Members[item].Email === entry) {
                         
-                        
                         arrayMemberId[i] = PeopleData.Members[item].MemberId;
-                        i=i+1;
                         
                         return db.collection('users').doc(`${PeopleData.Members[item].MemberId}`).get()
-                        .then(doc => {
-                            if (!doc.exists) {
-                                
-                                admin.auth().createUser({
-                                    uid: PeopleData.Members[item].MemberId.toString(),
-                                    email: PeopleData.Members[item].Email.trim(),
-                                    emailVerified: false,
-                                    password: 'test1234',
-                                    displayName: PeopleData.Members[item].Name,
-                                    disabled: false
-                                })
+                            .then(doc => {
+                                if (!doc.exists) {
+                                    admin.auth().createUser({
+                                        email: PeopleData.Members[item].Email.trim(),
+                                        emailVerified: false,
+                                        password: 'test1234',
+                                        photoURL: generatePhotoUrl(PeopleData.Members[item].Email.trim()),
+                                        // phoneNumber: ;
+                                        displayName: PeopleData.Members[item].Name,
+                                        disabled: false
+                                    })
                                     .then(function(userRecord) {
-                                    // See the UserRecord reference doc for the contents of userRecord.
-                                    console.log('Successfully created new user:', userRecord.uid);
+                                        // See the UserRecord reference doc for the contents of userRecord.
+                                        console.log('Successfully created new user:', userRecord.uid);
 
-                                    var tempUser = {
-                                            email: PeopleData.Members[item].Email,
-                                            name: PeopleData.Members[item].Name,
+                                        var tempUser = {
+                                            email: userRecord.email,
+                                            name: userRecord.displayName,
                                             id: PeopleData.Members[item].MemberId,
                                             peopleId: PeopleData.Members[item].MemberId,
                                             uid: Number(userRecord.uid),
@@ -65,8 +69,8 @@ module.exports = function() {
                                             //teams: [PeopleData.Members[item].TeamMemberships[0].TeamId]
                                         };
                                         
-                                    // Add user to firestore if admin
-                                        db.collection('users').doc(`${PeopleData.Members[item].MemberId}`).set(tempUser).then(ref => {
+                                        // Add user to firestore if admin
+                                        db.collection('users').doc(`${userRecord.uid}`).set(tempUser).then(ref => {
                                             console.log('User added: ', tempUser);
                                             return ref;
                                         }).catch(err => {
@@ -91,25 +95,18 @@ module.exports = function() {
                             console.log('Error getting document', err);
                             return err;
                             //throw new Error(`Use addTeams with the following inputs: teamId, teamName, TeamParentId, CopyOfTeamId.`); 
-                        }) 
-
-
+                        })
                     }
                 });
                 itemsProcessed++;
                 if(itemsProcessed === requiredListOwnerEmail.length) {
                     itemsProcessed = 0;
                     setTimeout(function() { callback(); }, 5000);
-                }
-                
+                }         
         });
-
-        
-
     });
 
     function callback () { 
-            
         arrayMemberId.forEach(function(entry){
             
             // People REST API: GetLists (Get all teams in people)
@@ -119,10 +116,9 @@ module.exports = function() {
                 //console.log('error:', error); // Print the error if one occurred
                 //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                 const PeopleData = JSON.parse(body);
-                module.exports = PeopleData;
             
                 // Loop through a whole team
-                Object.keys(PeopleData.Lists).forEach(function (item) {
+                Object.keys(PeopleData.Lists).forEach(function (item, j) {
                     
                         // Find team based on TeamId
                         if (PeopleData.Lists[item].MemberId === entry) {
@@ -136,7 +132,6 @@ module.exports = function() {
                             };
                         
                             arrayListId[j] = PeopleData.Lists[item].ListId;
-                            j=j+1;
 
                             db.collection('users').doc(`${entry}`).update({
                                 teams: FieldValue.arrayUnion(PeopleData.Lists[item].ListId)
@@ -182,12 +177,10 @@ module.exports = function() {
         });
     }
 
-
     function callbackAdmin () { 
-
         // Loop through all admins and create. 
         requiredAdmins.forEach(function(entry) {
-
+            console.log('create.admin: ', `creating admin user with id ${entry}`);
             // **** OBS **** Still on people-VOL!!
             
             // People REST API: GetTeams (Get all teams in people)
@@ -197,24 +190,23 @@ module.exports = function() {
                     //console.log('error:', error); // Print the error if one occurred
                     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                     const PeopleData = JSON.parse(body);
-                    module.exports = PeopleData;
                         
                     // Find users that are admins / "Holdleder". If not users is "basis"
                     if (PeopleData.Member.MemberId === entry) {
                         
                         admin.auth().createUser({
-                            uid: PeopleData.Member.MemberId.toString(),
                             email: PeopleData.Member.Email,
                             emailVerified: false,
                             password: 'test1234',
+                            photoURL: generatePhotoUrl(PeopleData.Member.Email),
                             displayName: PeopleData.Member.Name,
                             disabled: false
                         })
                             .then(function(userRecord) {
-                            // See the UserRecord reference doc for the contents of userRecord.
-                            console.log('Successfully created new user:', userRecord.uid);
+                                // See the UserRecord reference doc for the contents of userRecord.
+                                console.log('Successfully created new user:', userRecord.uid);
 
-                            var tempUser = {
+                                var tempUser = {
                                     email: PeopleData.Member.Email,
                                     name: PeopleData.Member.Name,
                                     peopleId: PeopleData.Member.MemberId,
@@ -223,8 +215,8 @@ module.exports = function() {
                                     teams: arrayListId
                                 };
                                 
-                            // Add user to firestore if admin
-                                var addUser = db.collection('users').doc(`${PeopleData.Member.MemberId}`).set(tempUser).then(ref => {
+                                // Add user to firestore if admin
+                                var addUser = db.collection('users').doc(`${userRecord.uid}`).set(tempUser).then(ref => {
                                     return ref;
                                 }).catch(err => {
                                     console.log('Error getting document', err);
@@ -234,10 +226,8 @@ module.exports = function() {
                             
                             })
                             .catch(function(error) {
-                            console.log('Error creating new user:', error);
-                            });
-
-                        
+                                console.log('Error creating new user:', error);
+                            });                        
                     }
                 });
         });
