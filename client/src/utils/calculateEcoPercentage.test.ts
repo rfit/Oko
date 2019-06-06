456027bdf59a23aa603656a7d11b45dd397fc973
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2019-06-06 19:08:41 +0200

    Fix bug where we devide by 0

--- client/src/utils/calculateEcoPercentage.test.ts
+++ client/src/utils/calculateEcoPercentage.test.ts
@@ -3,5 +3,6 @@ import calculateEcoPercentage from './calculateEcoPercentage';
 describe('calculateEcoPercentage', () => {
     it('should calulate percentage corrently', () => {
 		expect( calculateEcoPercentage(90, 10, 10) ).toBe(90);
+		expect( calculateEcoPercentage(0, 0, 300) ).toBe(100);
 	});
 });
--- client/src/utils/calculateEcoPercentage.ts
+++ client/src/utils/calculateEcoPercentage.ts
@@ -1,13 +1,20 @@
-function calulateEcoPercentage (eco: number, nonEco: number, excluded: number) : number {
+/* calulateEcoPercentage
+ * While TypeScript ensures that it's numbers, we sometimes get the data from a value somewhere, this makes us get some string.
+ * With the following code we also ensure it's really numbers before we try to calculate.
+ */
+function calulateEcoPercentage (organicInput: number, nonOrganicInput: number, excludedInput: number) : number {
+	const organic = Number(organicInput);
+	const nonOrganic = Number(nonOrganicInput);
+	const excluded = Number(excludedInput);
+
 	// Total amount
-	const total = Number(eco) + Number(nonEco) + Number(excluded);
-	// Amount considered ECO
-	const ecoOrOk = Number(eco) + Number(excluded);
+	const total = organic + nonOrganic + excluded;
 
-	// console.log(total, ecoOrOk, eco, nonEco, excluded, Number(eco)/(total - Number(excluded)) * 100);
+	// Percent that is considered Organic.
+	// If the total amount is excluded, it's zero, and we can't continue as we would devide by zero
+	const percentage = total - excluded === 0 ? 100 : organic / (total - excluded) * 100;
 
-	// Percent that is considered ECO
-	return Number(eco)/(total - Number(excluded)) * 100;
+	return percentage;
 }
 
-export default calulateEcoPercentage
+export default calulateEcoPercentage;
