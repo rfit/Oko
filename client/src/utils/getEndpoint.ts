d773bcc49b5dd47090041619a9db357fbe62e67c
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2020-05-13 12:02:01 +0200

    Update getEndpoint.ts
    Always use staging for now

--- client/src/utils/getEndpoint.ts
+++ client/src/utils/getEndpoint.ts
@@ -1,14 +1,18 @@
 function getEndpoint(hostname: string) {
 	switch (hostname) {
-		case 'oko.roskilde-festival.dk':
-		case 'okoapp-production-2019.firebaseapp.com':
-			return 'https://europe-west1-okoapp-production-2019.cloudfunctions.net/api';
-		case 'oko-staging.roskilde-festival.dk':
-		case 'okoapp-staging.firebaseapp.com':
-			return 'https://europe-west1-okoapp-staging.cloudfunctions.net/api';
 		default:
-			return 'http://localhost:5000/okoapp-staging/europe-west1/api'
+			return 'https://europe-west1-okoapp-staging.cloudfunctions.net/api';
 	}
+	// switch (hostname) {
+	// 	case 'oko.roskilde-festival.dk':
+	// 	case 'okoapp-production-2019.firebaseapp.com':
+	// 		return 'https://europe-west1-okoapp-production-2019.cloudfunctions.net/api';
+	// 	case 'oko-staging.roskilde-festival.dk':
+	// 	case 'okoapp-staging.firebaseapp.com':
+	// 		return 'https://europe-west1-okoapp-staging.cloudfunctions.net/api';
+	// 	default:
+	// 		return 'http://localhost:5000/okoapp-staging/europe-west1/api'
+	// }
 }
 
 export default getEndpoint;
